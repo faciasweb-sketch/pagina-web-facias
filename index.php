891b37b997c77cb7766cb057fb856dbbebdbb33b
faciasweb-sketch <facias.videos@gmail.com>
2026-08-18 17:45:06 -0300

    Redirect Academica menu item to new academica.php page

--- index.php
+++ index.php
@@ -138,7 +138,7 @@ if ($files) {
                     </li>
                     <!-- Académica -->
                     <li class="nav-item dropdown">
-                        <a href="#academica" class="nav-link dropdown-toggle" id="nav-item-academica">
+                        <a href="academica.php" class="nav-link dropdown-toggle" id="nav-item-academica">
                             Académica
                             <svg class="dropdown-arrow" viewBox="0 0 20 20" fill="currentColor" width="12" height="12">
                                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
